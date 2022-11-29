20815cb4a7cd54cd253bd21f298ffdcb8d31efde
Maria Casola <mjcasola1988@gmail.com>
2022-11-29 13:29:25 -0300

    creacion del menu admin

--- frontend/src/pages/admin/Dashboard.js
+++ frontend/src/pages/admin/Dashboard.js
@@ -1,4 +1,5 @@
 import { useAuth } from "../../context/auth";
+import { NavLink } from "react-router-dom";
 import Jumbotron from "../../components/cards/Jumbotron";
 
 
@@ -8,9 +9,35 @@ export default function AdminDashboard(){
 
     return (
         <>
-            <Jumbotron title={`Holis ${auth?.user?.name}`} subTitle='Plataforma de Administrador'/>
-            <pre>{JSON.stringify(auth, null, 4)}</pre>
+            <Jumbotron title={`Holis ${auth?.user?.name}`} subTitle='Plataforma de Administrador'
+            />
 
+            <div className="container-fluid">
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="p-3 mt-2 mb-2 h4 bg-light">Admin Links</div>
+
+                        <ul className="list-group list-unstyled">
+                            <li>
+                                <NavLink className='list-group-item' to='/dashboard/admin/category'>
+                                    Crear Categoria
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink className='list-group-item' to='/dashboard/admin/product'>
+                                    Crear Producto
+                                </NavLink>
+                            </li>
+                        </ul>
+
+
+
+                        </div>
+                    <div className="col-md-9">Contenido</div>
+
+                 </div>
+            </div>
         </>
+
     );
 }
