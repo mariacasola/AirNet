a5819bd0b10161fdeb1ae26c2f3f13393ef4fa09
Maria Casola <mjcasola1988@gmail.com>
2022-12-02 19:52:02 -0300

    crud productos parte 1

--- frontend/src/pages/admin/Product.js
+++ frontend/src/pages/admin/Product.js
@@ -1,11 +1,42 @@
 import { useAuth } from "../../context/auth";
+import { useState, useEffect } from "react";
 import Jumbotron from "../../components/cards/Jumbotron";
 import AdminMenu from "../../components/nav/AdminMenu";
+import axios from "axios";
+import React from 'react';
+import { Select } from 'antd';
 
+const {Option} = Select;
 
 export default function AdminProduct(){
     // context
     const [auth, setAuth] = useAuth();
+    // state
+    const [categories, setCategories] = useState([]);
+    const [photo, setPhoto] = useState("");
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [shipping, setShipping] = useState("");
+    const [quantity, setQuantity] = useState("");
+
+
+
+    useEffect (() => {
+        loadCategories();
+    }, []);
+
+
+    const loadCategories = async () =>{
+        try {
+            const {data} = await axios.get("/categories");
+            setCategories(data);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
 
     return (
         <>
@@ -21,7 +52,23 @@ export default function AdminProduct(){
                     <div className="col-md-9">
                     <div className="p-3 mt-2 mb-2 h4 bg-light">Crear Producto</div>
 
-                       <p>Crear Producto desde</p>
+
+                    <Select
+                    showSearch
+                    bordered={false} 
+                    size="large" 
+                    className="form-select mb-3" 
+                    placeholder="Seleccione una categoria" 
+                    onChange={(value) =>{}}
+                    >
+                        {categories?.map((c) => (
+                        <Option key={c._id} value={c.name}>
+                            {c.name}
+                        </Option>))}
+                    </Select>
+
+                    
+                       
                     </div>
 
                  </div>
