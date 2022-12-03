a332a4a47c2b6e5bd4d82a0aae2a8b4525c31b22
Maria Casola <mjcasola1988@gmail.com>
2022-12-03 13:33:01 -0300

    crud de productos

--- frontend/src/pages/admin/Product.js
+++ frontend/src/pages/admin/Product.js
@@ -5,6 +5,8 @@ import AdminMenu from "../../components/nav/AdminMenu";
 import axios from "axios";
 import React from 'react';
 import { Select } from 'antd';
+import toast from "react-hot-toast";
+import { useNavigate } from "react-router-dom";
 
 const {Option} = Select;
 
@@ -21,6 +23,9 @@ export default function AdminProduct(){
     const [shipping, setShipping] = useState("");
     const [quantity, setQuantity] = useState("");
 
+    // hook 
+    const navigate = useNavigate();
+
 
 
     useEffect (() => {
@@ -36,6 +41,34 @@ export default function AdminProduct(){
             console.log(err);
         }
     }
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try{
+            const productData = new FormData();
+            productData.append("photo", photo);
+            productData.append("name", name);
+            productData.append("description", description);
+            productData.append("price", price);
+            productData.append("category", category);
+            productData.append("shipping", shipping);
+            productData.append("quantity", quantity);
+
+            const {data} = await axios.post('/product', productData);
+            if (data?.error) {
+                toast.error(data.error);
+            } else {
+                toast.success(`"${data.name}" ha sido creado`);
+                navigate("/dashboard/admin/products");
+            }
+        
+        } catch (err) {
+            toast.error("No se creo el producto")
+    }
+};
+
+    
 
 
     return (
@@ -80,9 +113,31 @@ export default function AdminProduct(){
                         </label>
                     </div>
 
+                    <input 
+                    type='text' 
+                    className="form-control p-2 mb-3" placeholder="Ingrese un nombre"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    />
+
+                    <textarea 
+                    type='text' 
+                    className="form-control p-2 mb-3" placeholder="Ingrese una descripcion"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                    />
+
+
+                    <input 
+                    type="number" 
+                    className="form-control p-2 mb-3" placeholder="Ingrese el precio"
+                    value={price}
+                    onChange={(e) => setPrice(e.target.value)}
+                    />
+
 
                     <Select
-                    showSearch
+                    //showSearch
                     bordered={false} 
                     size="large" 
                     className="form-select mb-3" 
@@ -90,11 +145,34 @@ export default function AdminProduct(){
                     onChange={(value) => setCategory(value)}
                     >
                         {categories?.map((c) => (
-                        <Option key={c._id} value={c.name}>
+                        <Option key={c._id} value={c._id}>
                             {c.name}
                         </Option>))}
                     </Select>
 
+
+                    <Select
+                    bordered={false} 
+                    size="large" 
+                    className="form-select mb-3" 
+                    placeholder="Seleccione Envio" 
+                    onChange={(value) => setShipping(value)}
+                    >
+                        <Option value="0">No</Option>
+                        <Option value="1">Si</Option>
+
+                    </Select>
+
+
+
+                    <input 
+                    type="number" 
+                    min="1"
+                    className="form-control p-2 mb-3" placeholder="Ingrese la cantidad"
+                    value={quantity}
+                    onChange={(e) => setQuantity(e.target.value)}
+                    />
+                    <button onClick={handleSubmit} className="btn btn-primary mb-5">Enviar</button>
                     
                        
                     </div>
