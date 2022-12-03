3e8b596fc9f372eeea26d179dd50236cafadc2ee
Maria Casola <mjcasola1988@gmail.com>
2022-12-03 14:32:22 -0300

    renderizar productos

--- frontend/src/pages/admin/Products.js
+++ frontend/src/pages/admin/Products.js
@@ -3,6 +3,8 @@ import { useAuth } from "../../context/auth";
 import Jumbotron from "../../components/cards/Jumbotron";
 import AdminMenu from "../../components/nav/AdminMenu";
 import axios from "axios";
+import { Link } from "react-router-dom";
+import moment from "moment";
 
 
 export default function AdminProducts(){
@@ -40,7 +42,39 @@ export default function AdminProducts(){
                     <div className="col-md-9">
                     <div className="p-3 mt-2 mb-2 h4 bg-light">Productos</div>
 
-                    show list of products...{products.length}
+                    {products?.map ((p) => (
+                    <Link 
+                    key={p._id} 
+                    to={`/dashboard/admin/product/update/${p.slug}`}
+                    >
+
+                        <div className="card mb-3">
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                    <img 
+                                        src={`${process.env.REACT_APP_API}/product/photo/${p._id}`} 
+                                        alt={p.name}
+                                        className="img img-fluid rounded-start"
+                                    />
+                                </div>
+
+                                <div className="col-md-8 ">
+                                    <div className="card-body">
+                                        <h5 className="card-title">{p.name}</h5>
+                                        <p className="card-text">{p.description}</p>
+                                        <p className="card-text">
+                                            <small className="text-muted">
+                                                {moment(p.createdAt).format("Do MMMM YYYY, h:mm:ss a")}</small>
+                                        </p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    
+                    
+                    </Link>
+                    ))}
                        
                     </div>
 
