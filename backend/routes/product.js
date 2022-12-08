978740357244b220fbbf61dcf349b74bf41b7096
Maria Casola <mjcasola1988@gmail.com>
2022-12-08 19:38:14 -0300

    desplegar mas productos en pagina

--- backend/routes/product.js
+++ backend/routes/product.js
@@ -14,6 +14,8 @@ import {
     remove, 
     update,
     filteredProducts,
+    productsCount,
+    listProducts,
  } from '../controllers/product.js';
 
 router.post('/product', requireSignin, isAdmin, formidable(), create);
@@ -22,7 +24,9 @@ router.get('/product/:slug', read);
 router.get('/product/photo/:productId', photo);
 router.delete('/product/:productId', requireSignin, isAdmin, remove);
 router.put('/product/:productId', requireSignin, isAdmin, formidable(), update);
-router.post('/filtered-products', filteredProducts)
+router.post('/filtered-products', filteredProducts);
+router.get("/products-count", productsCount);
+router.get("/list-products/:page", listProducts);
 
 
 export default router;
