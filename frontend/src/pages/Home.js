ea679979e0c1a0c0243f0c815365e7e3db79a55b
Maria Casola <mjcasola1988@gmail.com>
2022-12-08 12:56:11 -0300

    productos card

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,14 +1,61 @@
+import { useEffect, useState } from "react";
 import Jumbotron from "../components/cards/Jumbotron"; 
-import { useAuth } from "../context/auth"; 
+import axios from "axios";
+import moment from "moment";
 
 export default function Home() {
+  const [products, setProducts] = useState([]);
 
-    const [auth, setAuth] = useAuth();
+  useEffect(() => {
+    loadProducts();
+  }, []);
+
+  const loadProducts = async () => {
+    try {
+      const { data } = await axios.get("/products");
+      setProducts(data);
+    } catch (err) {
+      console.log(err)
+    }
+  };
+
+  const arr = [...products];
+  const sortedBySold = arr?.sort((a,b) => (a.sold <b.sold ? 1 : -1));
+
+
     return (
         <div>
-          <Jumbotron title="Hello World"/>
-          <pre>{JSON.stringify(auth, null, 4)}</pre>
-  
+          <Jumbotron title="Hola a todos" subTitle="Bienvenidos a Airnet"/>
+
+          <div className="row">
+            <div className="col-md-6">
+              <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">Lo ultimo!</h2>
+              {products?.map (p => (
+              <div>
+                <p>{p.name}</p>
+                <p>{moment(p.createdAt).fromNow()}</p>
+                <p>{p.sold}</p>
+              </div>
+            ))}
+            </div>
+
+            <div className="col-md-6">
+              <h2 className="p-3 mt-2 mb-2 h4 bg-light text-center">Los mas vendidos!</h2>
+            {products?.map (p => (
+              <div>
+                <p>{p.name}</p>
+                <p>{moment(p.createdAt).fromNow()}</p>
+                <p>{p.sold}</p>
+              </div>
+            ))}
+            </div>
+
+          </div>
+
+          
+
+         
+
       </div>
     );
-  }
+}
