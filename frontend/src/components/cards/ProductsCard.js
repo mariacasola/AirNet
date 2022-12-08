abccfad7485fac8b7cf4e973dda6a45204a97f28
Maria Casola <mjcasola1988@gmail.com>
2022-12-08 13:39:33 -0300

    productos card

--- frontend/src/components/cards/ProductsCard.js
+++ frontend/src/components/cards/ProductsCard.js
@@ -2,15 +2,32 @@ import moment from "moment";
 
 export default function ProductCard ({ p }) {
     return (
-        <div className="card mb-3">
+        <div className="card mb-3 hoverable">
             <img
             src={`${process.env.REACT_APP_API}/product/photo/${p._id}`}
             alt={p.name}
             style={{ height: "300px", objectFit: "cover"}}
             />
-            <p>{p.name}</p>
-            <p>{moment(p.createdAt).fromNow()}</p>
-            <p>{p.sold}</p>
+            <div className="card-body">
+                <h5>{p?.name}</h5>
+                <p className="card-text">{p?.description?.substring(0,60)}...</p>
+
+            </div>
+
+            <div className="d-flex justify-content-between">
+                <button className="btn btn-primary col card-button" 
+                style={{ borderBottomLeftRadius: '5px'}}>
+                    Ver Producto
+                </button>
+
+                <button className="btn btn-outline-primary col card-button"
+                 style={{ borderBottomRightRadius: '5px'}}>
+                    Añadir al carrito
+                </button>
+            </div>
+            
+            {/* <p>{moment(p.createdAt).fromNow()}</p>
+            <p>{p.sold}</p> */}
         </div>
     );
 }
